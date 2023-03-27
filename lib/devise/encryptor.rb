6aa7312d26975fc4f16afe438a20b3866d3bb087
Robert <rdober@crystalchain.io>
2023-03-27 10:17:45 +0200

    Replaced bcrypt with Argon2

--- lib/devise/encryptor.rb
+++ lib/devise/encryptor.rb
@@ -3,19 +3,39 @@
 require 'bcrypt'
 
 module Devise
-  module Encryptor
-    def self.digest(klass, password)
-      if klass.pepper.present?
-        password = "#{password}#{klass.pepper}"
-      end
-      BCryptEncryptor.create_password(password, cost: klass.stretches)
+  module Encryptor 
+    extend self
+    def digest(klass, password)
+      Rails.logger.info('ARGON2 Password Creation')
+      hasher = Argon2::Password.new(m_cost:, p_cost:, secret:, t_cost:)
+      hasher.create(password)
     end
 
-    def self.compare(klass, hashed_password, password)
-      return false if hashed_password.blank?
+    def compare(klass, hashed_password, password)
+      Rails.logger.info('ARGON2 Password Verification')
+      Argon2::Password.verify_password(
+        "#{password}#{klass.pepper}",
+        hashed_password,
+        secret
+      )
+    end
 
-      password = BCryptEncryptor.compare_password("#{password}#{klass.pepper}", hashed_password)
-      Devise.secure_compare(password, hashed_password)
+    private
+
+    def m_cost
+      @__m_cost__ ||= Rails.application.secrets.argon2_m_cost || 20
+    end
+
+    def p_cost
+      @__p_cost__ ||= 1
+    end
+
+    def secret
+      @__secret__ ||= Rails.application.secrets.argon2_secret || '4gcnu3X1Kmuz5p8woCeeRSVr6NJFHAwFoEHqfQWeRA8' 
+    end
+
+    def t_cost
+      @__t_cost__ ||= 2
     end
   end
 end
